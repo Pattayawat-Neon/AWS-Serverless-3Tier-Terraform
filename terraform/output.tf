e9505324df9e4035664cec9eee77b7edc8c977cf
Pattayawat <pattayawat.poo@dome.tu.ac.th>
2026-01-15 10:56:57 +0700

    debug: remove cloudfront

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,6 @@ output "api_url" {
   value = aws_apigatewayv2_api.api.api_endpoint
 }
 
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.frontend.domain_name
+output "s3_website_url" {
+  value = aws_s3_bucket_website_configuration.frontend_website.website_endpoint
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -46,9 +46,9 @@ resource "aws_s3_bucket" "frontend_bucket" {
 resource "aws_s3_bucket_public_access_block" "frontend_block" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
@@ -59,21 +59,26 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "AllowCloudFrontAccess"
+        Sid       = "PublicReadGetObject"
         Effect    = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
-          }
-        }
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
 resource "aws_s3_object" "create_zip" {
